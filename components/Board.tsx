a3014154fb969b7e0b9d922acec7a55a931cbaf7
Akis-Hajittofi <akis.hajittofi@hotmail.co.uk>
2023-09-26 19:02:57 +0100

    Drag&drop now working fully and updates DB. Search also implemented.

--- components/Board.tsx
+++ components/Board.tsx
@@ -6,10 +6,14 @@ import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import Column from "./Column";
 
 function Board() {
-  const [board, getBoard] = useBoardStore((state) => [
-    state.board,
-    state.getBoard,
-  ]);
+  const [board, getBoard, setBoardState, updateTodoInDB] = useBoardStore(
+    (state) => [
+      state.board,
+      state.getBoard,
+      state.setBoardState,
+      state.updateTodoInDB,
+    ]
+  );
 
   useEffect(() => {
     getBoard();
@@ -17,7 +21,86 @@ function Board() {
 
   console.log("Board.tsx", board);
 
-  const handleOnDragEnd = (result: DropResult) => {};
+  const handleOnDragEnd = (result: DropResult) => {
+    const { destination, source, type } = result;
+
+    // Do nothing if the user drags card outside of the board
+    if (!destination) return;
+
+    // Handle dragging a column
+    if (type === "column") {
+      const entries = Array.from(board.columns.entries());
+      const [removed] = entries.splice(source.index, 1); // Splice out of where I am removing it from
+      entries.splice(destination.index, 0, removed); // Splice the removed item into the other column
+      const rearrangedColumns = new Map(entries); // Create a new map of the rearranged columns
+
+      setBoardState({
+        ...board,
+        columns: rearrangedColumns,
+      });
+    }
+
+    // Changed the index's to the ID's from React Beautiful DND library
+    // It rebuilds the columns with the correct arrays
+    // Giving you the columns you need to modify
+
+    const columns = Array.from(board.columns);
+    const startColIndex = columns[Number(source.droppableId)];
+    const finishColIndex = columns[Number(destination.droppableId)];
+
+    const startCol: Column = {
+      id: startColIndex[0],
+      todos: finishColIndex[1].todos,
+    };
+
+    const finishCol: Column = {
+      id: finishColIndex[0],
+      todos: finishColIndex[1].todos,
+    };
+
+    // Do nothing if the source and finish is the same (Drag & drop in same place)
+    if (!startCol || !finishCol) return;
+    if (source.index === destination.index && startCol === finishCol) return;
+
+    const newTodos = startCol.todos;
+    const [todoMoved] = newTodos.splice(source.index, 1);
+
+    if (startCol.id === finishCol.id) {
+      // Dragging within the same column
+      newTodos.splice(destination.index, 0, todoMoved);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+      const newColumns = new Map(board.columns);
+      newColumns.set(startCol.id, newCol);
+
+      setBoardState({ ...board, columns: newColumns });
+    } else {
+      // Dragging to a different column
+      // Need to make the correct changes in start and finish column to reflect the interaction
+
+      const finishTodos = Array.from(finishCol.todos);
+      finishTodos.splice(destination.index, 0, todoMoved);
+
+      const newColumns = new Map(board.columns);
+      const newCol = {
+        id: startCol.id,
+        todos: newTodos,
+      };
+
+      newColumns.set(startCol.id, newCol);
+      newColumns.set(finishCol.id, {
+        id: finishCol.id,
+        todos: finishTodos,
+      });
+      // TODO: Update the DB based on the order of todos, rather than only when they change status
+
+      updateTodoInDB(todoMoved, finishCol.id);
+      setBoardState({ ...board, columns: newColumns });
+    }
+  };
+
   return (
     <DragDropContext onDragEnd={handleOnDragEnd}>
       <Droppable droppableId="board" direction="horizontal" type="column">
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,13 @@ import Image from "next/image";
 import React from "react";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/24/solid";
 import Avatar from "react-avatar";
+import { useBoardStore } from "@/store/BoardStore";
 
 function Header() {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+    state.searchString,
+    state.setSearchString,
+  ]);
   return (
     <header>
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
@@ -36,9 +41,12 @@ function Header() {
         <div className="flex items-center space-x-5 flex-1 justify-end w-full">
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 text-gray-400" />
+            {/* Could make it so that it only searches after 500ms  after user types etc rather than constantly */}
             <input
               type="text"
               placeholder="Search"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
               className="flex-1 outline-none p-2"
             />
             <button type="submit" hidden></button>
@@ -48,7 +56,6 @@ function Header() {
         </div>
       </div>
 
-      {/* TODO: 2:02:30 AI Suggestions */}
       <div className="flex items-center justify-center px-5 py-2 md:py-5">
         <p className="flex items-center p-5 text-sm font-light pr-5 shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
           <UserCircleIcon className="inline-block h-10 w-10 text-[#0055D1] mr-1" />
